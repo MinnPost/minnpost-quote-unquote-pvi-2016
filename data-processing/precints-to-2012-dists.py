1cb8dc7f4f21a633bc397a57bf53023af26ad55a
Tom Nehil <tnehil@minnpost.com>
2016-07-18 13:48:13 -0500

    Lots of code to output 2010 results in 2012 districts

--- data-processing/precints-to-2012-dists.py
+++ data-processing/precints-to-2012-dists.py
@@ -5,7 +5,7 @@ import csv
 with open('../data/2010_general_results_final.csv','r') as f:
     elex_data = csv.DictReader(f)
 
-    precinct_results = []
+    precinct_results = {}
 
     for row in elex_data:
 
@@ -27,8 +27,7 @@ with open('../data/2010_general_results_final.csv','r') as f:
         house_gop = int(row['MNLEGR'])
         house_tot = int(row['MNLEGTOT'])
 
-        precinct_results.append(
-                                {
+        precinct_results[precinct_code] = {
                                     "precinct": precinct_code,
                                     "senate_dfl": senate_dfl,
                                     "senate_gop": senate_gop,
@@ -38,12 +37,71 @@ with open('../data/2010_general_results_final.csv','r') as f:
                                     "house_gop": house_gop,
                                     "house_tot": house_tot,
                                 }
-                               )
+results = {}
 
 with open('2010-precincts-in-2012-districts.csv') as f:
-    #do stuff
+    precincts = csv.reader(f)
 
-senate_districts = [str(n+1) for n in range(67)]
+    next(precincts) #skip header
 
-house_districts = [str(n+1)+"A" for n in range(67)]
-house_districts += [str(n+1)+"B" for n in range(67)]
+    for row in precincts:
+        precinct = row[0]
+        house_district = row[2]
+        senate_district = house_district[0:2]
+
+        #add to house
+        if house_district in results:
+            results[house_district]["gop_total"] += precinct_results[precinct]["house_gop"]
+            results[house_district]["dfl_total"] += precinct_results[precinct]["house_dfl"]
+            results[house_district]["vot_total"] += precinct_results[precinct]["house_tot"]
+            results[house_district]["tot_pcts"] += 1
+        else:
+            results[house_district] = {
+                                        "gop_total": precinct_results[precinct]["house_gop"],
+                                        "dfl_total": precinct_results[precinct]["house_dfl"],
+                                        "vot_total": precinct_results[precinct]["house_tot"],
+                                        "tot_pcts": 1
+                                      }
+
+        #add to senate
+        if senate_district in results:
+            results[senate_district]["gop_total"] += precinct_results[precinct]["senate_gop"]
+            results[senate_district]["dfl_total"] += precinct_results[precinct]["senate_dfl"]
+            results[senate_district]["vot_total"] += precinct_results[precinct]["senate_tot"]
+            results[senate_district]["tot_pcts"] += 1
+        else:
+            results[senate_district] = {
+                                        "gop_total": precinct_results[precinct]["senate_gop"],
+                                        "dfl_total": precinct_results[precinct]["senate_dfl"],
+                                        "vot_total": precinct_results[precinct]["senate_tot"],
+                                        "tot_pcts": 1
+                                      }
+
+#save results to file
+with open('2010_results_in_2012_districts.csv','w') as of:
+    out = csv.writer(of)
+
+    out.writerow(["district", "total_precincts", "gop_votes", "dfl_votes","total_votes", "gop%", "dfl%", "margin"])
+
+    for district in results:
+
+        total_precincts = results[district]["tot_pcts"]
+        gop_total = results[district]["gop_total"]
+        dfl_total = results[district]["dfl_total"]
+        vot_total = results[district]["vot_total"]
+
+        gop_percent = float(gop_total) / float(vot_total)
+        dfl_percent = float(dfl_total) / float(vot_total)
+
+        margin = dfl_percent - gop_percent
+
+        out.writerow([
+                        district,
+                        total_precincts,
+                        gop_total,
+                        dfl_total,
+                        vot_total,
+                        gop_percent,
+                        dfl_percent,
+                        margin
+                     ])
